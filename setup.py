91c1d1fc362b6c594f1e18036fed53f64450132b
RF-Tar-Railt <3165388245@qq.com>
2022-05-22 20:32:14 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="arclet-alconna-graia",
-    url="https://github.com/ArcletProject/Alconna/blob/master/arclet/alconna/graia",
-    version="0.0.12",
-    author="ArcletProject",
+    name="raian-bot",
+    url="https://github.com/RF-Tar-Railt/RaianBot",
+    version="0.1",
+    author="RF-Tar-Railt",
     author_email="rf_tar_railt@qq.com",
-    description="Support Alconna to GraiaProject",
+    description="simple qq bot bases on Araidne and Alconna",
     license='AGPL-3.0',
-    packages=['arclet.alconna.graia'],
-    install_requires=['graia-ariadne', 'graia-broadcast', 'arclet-alconna'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU Affero General Public License v3",
@@ -18,11 +16,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    keywords=['alconna', 'graia', 'dispatcher', 'arclet'],
     python_requires='>=3.8',
-    project_urls={
-        'Documentation': 'https://arcletproject.github.io/docs/alconna/tutorial',
-        'Bug Reports': 'https://github.com/ArcletProject/Alconna/issues',
-        'Source': 'https://github.com/ArcletProject/Alconna',
-    },
 )
