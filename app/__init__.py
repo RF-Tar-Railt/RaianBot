d42e68e46440d8d10acc8c451bf4c9ce07061cc4
RF-Tar-Railt <3165388245@qq.com>
2022-07-17 14:59:26 +0800

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from .core import RaianMain
 from .config import load_config
 from .data import BotDataManager
 from .control import require_admin, require_function
-from .plugin import Sender, Target, command, record, meta, schedule
+from .plugin import Sender, Target, record, meta, schedule
