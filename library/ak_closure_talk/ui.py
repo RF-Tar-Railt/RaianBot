8d99ebc42fc90b46291c8bc65163e7f9885a3890
Null <49110142+nullqwertyuiop@users.noreply.github.com>
2023-01-31 02:55:57 +0800

    Fixed hyperlink wrapping

--- library/ak_closure_talk/ui.py
+++ library/ak_closure_talk/ui.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
 import re
+from typing import List, TypeVar
 
 from lxml.builder import E
 
 HYPERLINK_PATTERN = re.compile(r"https?://\S+")
+_T = TypeVar("_T")
 
 
-def wrap_text(text: str | list, newline: bool = True, hyperlink: bool = True):
+def wrap_text(text: str | list, newline: bool = True, hyperlink: bool = True) -> list:
     wrapped = [text] if isinstance(text, str) else text
     if hyperlink:
         wrapped = _add_hyperlink(wrapped)
@@ -16,7 +18,7 @@ def wrap_text(text: str | list, newline: bool = True, hyperlink: bool = True):
     return wrapped
 
 
-def _newline_to_br(text: list):
+def _newline_to_br(text: List[_T]) -> List[_T]:
     wrapped = []
     for part in text:
         if not isinstance(part, str):
@@ -29,14 +31,7 @@ def _newline_to_br(text: list):
     return wrapped
 
 
-def _replace_with_hyperlink(text: str, link: str):
-    wrapped = []
-    for piece in text.split(link):
-        wrapped.extend((piece, E.a(link, href=link)))
-    return wrapped[:-1]
-
-
-def _add_hyperlink(text: list):
+def _add_hyperlink(text: List[_T]) -> List[_T]:
     wrapped = []
     for part in text:
         if not isinstance(part, str):
@@ -45,8 +40,9 @@ def _add_hyperlink(text: list):
         if not (urls := HYPERLINK_PATTERN.findall(part)):
             wrapped.append(part)
             continue
-        parts = []
-        for url in urls:
-            parts.extend(_replace_with_hyperlink(part, url))
-        wrapped.extend(parts)
+        urls = [E.a(link, href=link) for link in urls]
+        parts = re.split(HYPERLINK_PATTERN, part)
+        for _part, url in zip(parts, urls):
+            wrapped.extend((_part, url))
+        wrapped.append(parts[-1])
     return wrapped
