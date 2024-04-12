38744a0852087b3aa09ed9cb080cb43b057469b9
rf_tar_railt <3165388245@qq.com>
2024-04-12 22:19:41 +0800

    :sparkles: add plugin `shell`

---
+++ plugins/shell/main.py
@@ -0,0 +1,56 @@
+import asyncio
+
+from avilla.core import MessageReceived, Context, MessageChain, Picture, RawResource
+from graia.saya.builtins.broadcast.shortcut import listen
+
+from arclet.alconna.graia import startswith
+
+from app.image import md2img
+from app.shortcut import permission, exclusive, accessable
+
+
+@listen(MessageReceived)
+@permission("master")
+@startswith("shell", bind="echos")
+@exclusive
+@accessable
+async def shell(ctx: Context, echos: MessageChain):
+    process = await asyncio.create_subprocess_shell(
+        str(echos),
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    try:
+        stdout, stderr = await asyncio.wait_for(process.communicate(), 20)
+    except asyncio.TimeoutError:
+        process.kill()
+        stdout, stderr = await process.communicate()
+    if stdout:
+        try:
+            res = stdout.decode("utf-8").strip()
+        except UnicodeDecodeError:
+            res = stdout.decode("gbk").strip()
+    elif stderr:
+        try:
+            res = stderr.decode("utf-8").strip()
+        except UnicodeDecodeError:
+            res = stderr.decode("gbk").strip()
+    else:
+        res = "No output"
+    md = f"""\
+> exit code: {process.returncode}
+
+```sh
+{res}
+```
+"""
+    return await ctx.scene.send_message(
+        Picture(
+            RawResource(
+                await md2img(
+                    md,
+                    max(max(len(i.strip()) for i in md.splitlines()) * 14, 240)
+                )
+            )
+        )
+    )
